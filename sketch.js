e8cafe08ae19c554a61519d27983a504b61f1161
ScienceMarc <24737495+ScienceMarc@users.noreply.github.com>
2020-03-11 13:27:49 -0500

    Added code which handles and displays functions which are entered using the new UI.

--- sketch.js
+++ sketch.js
@@ -1,7 +1,8 @@
 class FunctionObject { //Contains the function as well as the color of the line.
-	constructor(f) {
+	constructor(f, input) {
 		this.f = f;
-		this.color = color(random(0,255),random(0,255),random(0,255)); //TODO: come up with a better random color function which ensures good contrast.
+		this.color = color(random(0, 255), random(0, 255), random(0, 255)); //TODO: come up with a better random color function which ensures good contrast.
+		this.input = input;
 	}
 }
 
@@ -15,77 +16,111 @@ function setup() {
 
 function draw() {
 	background(20);
-	
+
 	push()
 	strokeWeight(2);
 	stroke(40)
 	//TODO: add support for polar coordinates and warping due to transformations
-	for (let i = -round((windowWidth/2)/scale); i < round((windowWidth/2)/scale) + 1; i++) { //X axis subdivision lines
-		line(i*scale + windowWidth/2,0,i*scale + windowWidth/2,windowHeight);
+	for (let i = -round((windowWidth / 2) / scale); i < round((windowWidth / 2) / scale) + 1; i++) { //X axis subdivision lines
+		line(i * scale + windowWidth / 2, 0, i * scale + windowWidth / 2, windowHeight);
 	}
-	for (let i = -round((windowHeight/2)/scale); i < round((windowHeight/2)/scale) + 1; i++) { //Y axis subdivision lines
-		line(0, i*scale + windowHeight/2, windowWidth, i*scale + windowHeight/2);
+	for (let i = -round((windowHeight / 2) / scale); i < round((windowHeight / 2) / scale) + 1; i++) { //Y axis subdivision lines
+		line(0, i * scale + windowHeight / 2, windowWidth, i * scale + windowHeight / 2);
 	}
 	stroke("gray");
-	line(0,windowHeight/2,windowWidth,windowHeight/2); //Horizontal line
-	line(windowWidth/2,0,windowWidth/2,windowHeight); //Vertical line
+	line(0, windowHeight / 2, windowWidth, windowHeight / 2); //Horizontal line
+	line(windowWidth / 2, 0, windowWidth / 2, windowHeight); //Vertical line
 	pop()
 	push()
 	fill("white")
 	stroke("black")
-	text(round(frameRate()),0,10); //TODO: hide later
-	for (let i = -round((windowWidth/2)/scale); i < round((windowWidth/2)/scale) + 1; i++) { //X axis units 
-		text(round(i*10)/10,i*scale + windowWidth/2,windowHeight/2 + 10); //TODO: Add support for variable increments
+	text(round(frameRate()), 0, 10); //TODO: hide later
+	for (let i = -round((windowWidth / 2) / scale); i < round((windowWidth / 2) / scale) + 1; i++) { //X axis units 
+		text(round(i * 10) / 10, i * scale + windowWidth / 2, windowHeight / 2 + 10); //TODO: Add support for variable increments
 	}
-	for (let i = -round((windowHeight/2)/scale); i < round((windowHeight/2)/scale) + 1; i++) { //Y axis units
-		if (-round(i*10)/10 == 0) {continue;}
-		text(-round(i*10)/10,windowWidth/2 + 1, i*scale + windowHeight/2 + 3); //TODO: add support for the complex plane and polar coordinates
+	for (let i = -round((windowHeight / 2) / scale); i < round((windowHeight / 2) / scale) + 1; i++) { //Y axis units
+		if (-round(i * 10) / 10 == 0) { continue; }
+		text(-round(i * 10) / 10, windowWidth / 2 + 1, i * scale + windowHeight / 2 + 3); //TODO: add support for the complex plane and polar coordinates
 	}
 	pop()
 	stroke("white");
-	
+
 	strokeWeight(2);
-	////scale = frameCount/10;
+	//scale = frameCount/10;
 	for (let i = 0; i < functions.length; i++) { //Plot every function in the list
-		plot(functions[i].f,functions[i].color)
+		plot(functions[i].f, functions[i].color)
 	}
 	UI();
 }
 
-function plot(f,color) {
+function plot(f, color) {
 	if (typeof f != "function") {
 		return;
 	}
 	let points = [];
-	for (let i = -(windowWidth/2)/scale; i < (windowWidth/2)/scale + 1; i+=1/(Math.ceil(scale/2))) { //Computes all of the points along the function
-		points.push(createVector((i*scale)+(windowWidth/2),-(f(i)*scale)+windowHeight/2,i)); //TODO: add support for complex values
+	for (let i = -(windowWidth / 2) / scale; i < (windowWidth / 2) / scale + 1; i += 1 / (Math.ceil(scale / 4))) { //Computes all of the points along the function
+		points.push(createVector((i * scale) + (windowWidth / 2), -(f(i) * scale) + windowHeight / 2, i)); //TODO: add support for complex values
 	}
-	for (let i = 0; i < points.length - (points.length%2) - 1; i++) {
-		let grad = (f(points[i].z + 0.0001) - f(points[i].z))/0.0001; //Finds the dervative of the curret point.
+	for (let i = 0; i < points.length - (points.length % 2) - 1; i++) {
+		let grad = (f(points[i].z + 0.0001) - f(points[i].z)) / 0.0001; //Finds the dervative of the curret point.
 		if (Math.abs(grad) > 200 || Number.isNaN(grad)) { //If the line is too steep or invalid make the line invisible
-			stroke(0,0,0,0);
+			stroke(0, 0, 0, 0);
 		}
 		else { //Otherwise draw it using the correct color
 			stroke(color);
 		}
-		line(points[i].x,points[i].y,points[i+1].x,points[i+1].y); //Links the calculated points with lines to smooth out the curve
+		line(points[i].x, points[i].y, points[i + 1].x, points[i + 1].y); //Links the calculated points with lines to smooth out the curve
 	}
 }
 
 let scope = {}
 function addFunction(f) {
-	functions.push(new FunctionObject(math.evaluate(f,scope))); //Allows adding functions using a string
-														        //TODO: add support to use this without the console.
+	functions.push(new FunctionObject(math.evaluate(f, scope),f)); //Allows adding functions using a string
+	//TODO: add support to use this without the console.
 }
 
-function UI() {
-	push()
-	stroke("gray");
-	fill("white");
 
-	rect(0,0,windowWidth*0.2,windowHeight);
-	fill("black");
+let isTyping = false;
+let typedText = "";
+function UI() {
+	push();
+	stroke(100);
+	fill(50);
+
+	rect(0, 0, windowWidth * 0.2, windowHeight);
+	fill("white");
 	textSize(24);
-	text("+", windowWidth*0.2 - 24,24);
+	text("+", windowWidth * 0.2 - 24, 24);
+	for (let i = 0; i < functions.length; i++) {
+		text(functions[i].input, 0, 50 + 50*i);
+	}
+	if (isTyping) {
+		text(typedText, 0, 50 + 50*functions.length);
+	}
 	pop();
+}
+
+function mouseClicked() {
+	if (mouseX >= windowWidth * 0.2 - 24 && mouseX <= windowWidth * 0.2 && mouseY >= 5 && mouseY <= 24) {
+		isTyping = true;
+	}
+	console.log(mouseX)
+}
+
+function keyPressed() {
+	if (isTyping) {
+		if (keyCode == BACKSPACE) {
+			typedText = typedText.substring(0, typedText.length - 1);
+		}
+		if (keyCode == ENTER) {
+			addFunction(typedText);
+			typedText = "";
+			isTyping = false;
+		}
+	}
+}
+function keyTyped() {
+	if (isTyping) {
+		typedText += key;
+	}
 }
