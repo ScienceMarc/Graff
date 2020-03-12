af269d4fcc4955e2457451a769a4af0f8b9b2ab6
ScienceMarc <sciencemarc@github.com>
2020-03-11 22:49:16 -0500

    Fixed bugs and added scalable axes based on zoom

--- sketch.js
+++ sketch.js
@@ -8,7 +8,7 @@ class FunctionObject { //Contains the function as well as the color of the line.
 
 let scale = 40; //The amount that the chart is zoomed in by. The higher, the more zommed in. Small numbers lead to problems
 let functions = [];
-let scope = {}
+let scope = {};
 
 
 function setup() {
@@ -18,39 +18,51 @@ function setup() {
 function draw() {
 	background(20);
 
-	push()
+	push();
 	strokeWeight(2);
-	stroke(40)
+	let increment = max(1,Math.round(40/scale))/Math.ceil(scale/200)
 	//TODO: add support for polar coordinates and warping due to transformations
-	for (let i = -round((windowWidth / 2) / scale); i < round((windowWidth / 2) / scale) + 1; i++) { //X axis subdivision lines
+	for (let i = -round((windowWidth / 2) / scale); i < round((windowWidth / 2) / scale) + 1; i+=increment) { //X axis subdivision lines
+		if ((round(i * 10) / 10)%1!=0) {
+			stroke(40);
+		}
+		else {
+			stroke(80);
+		}
 		line(i * scale + windowWidth / 2, 0, i * scale + windowWidth / 2, windowHeight);
 	}
-	for (let i = -round((windowHeight / 2) / scale); i < round((windowHeight / 2) / scale) + 1; i++) { //Y axis subdivision lines
+	for (let i = -round((windowHeight / 2) / scale); i < round((windowHeight / 2) / scale) + 1; i+=increment) { //Y axis subdivision lines
+		if ((round(i * 10) / 10)%1!=0) {
+			stroke(40);
+		}
+		else {
+			stroke(80);
+		}
 		line(0, i * scale + windowHeight / 2, windowWidth, i * scale + windowHeight / 2);
 	}
-	stroke("gray");
+	stroke(150);
 	line(0, windowHeight / 2, windowWidth, windowHeight / 2); //Horizontal line
 	line(windowWidth / 2, 0, windowWidth / 2, windowHeight); //Vertical line
-	pop()
-	push()
-	fill("white")
-	stroke("black")
+	pop();
+	push();
+	fill("white");
+	stroke("black");
 	text(round(frameRate()), 0, 10); //TODO: hide later
-	for (let i = -round((windowWidth / 2) / scale); i < round((windowWidth / 2) / scale) + 1; i++) { //X axis units 
-		text(round(i * 10) / 10, i * scale + windowWidth / 2, windowHeight / 2 + 10); //TODO: Add support for variable increments
+	for (let i = -round((windowWidth / 2) / scale); i < round((windowWidth / 2) / scale) + 1; i+=increment) { //X axis units 
+		text(round(i * 10) / 10, i * scale + windowWidth / 2 - 3, windowHeight / 2 + 10);
 	}
-	for (let i = -round((windowHeight / 2) / scale); i < round((windowHeight / 2) / scale) + 1; i++) { //Y axis units
+	for (let i = -round((windowHeight / 2) / scale); i < round((windowHeight / 2) / scale) + 1; i+=increment) { //Y axis units
 		if (-round(i * 10) / 10 == 0) { continue; }
 		text(-round(i * 10) / 10, windowWidth / 2 + 1, i * scale + windowHeight / 2 + 3); //TODO: add support for the complex plane and polar coordinates
 	}
-	pop()
+	pop();
 	stroke("white");
 
 	strokeWeight(2);
 	////scale = frameCount/10;
 	for (let i = 0; i < functions.length; i++) { //Plot every function in the list
-		functions[i].points = []
-		plot(i, functions[i].color)
+		functions[i].points = [];
+		plot(i, functions[i].color);
 	}
 	UI();
 	//noLoop()
@@ -82,7 +94,8 @@ function plot(index, color) {
 
 
 function addFunction(f) {
-	let sanatized = f.replace(/={0}/,"f(x) = ").replace(/^y(\s)*/, "f(x) ");
+	let sanatized = f.replace(/^[=]/i,"f(x) = ").replace(/^y(\s)*/i, "f(x) ");
+	console.log(sanatized)
 	functions.push(new FunctionObject(math.evaluate(sanatized, scope),f)); //Allows adding functions using a string
 	//TODO: add support to use this without the console.
 }
@@ -137,8 +150,8 @@ function keyTyped() {
 }
 function mouseWheel(event) {
 	scale -= event.delta/10;
-	if (scale <= 0) {
-		scale = 5;
+	if (scale < 25) {
+		scale = 25;
 	}
 	//console.log(scale)
 	loop();
